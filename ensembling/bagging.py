27f8e1c9aa58a4331310cdfb94416d8f285e2cd2
Silu Panda <31051721+SiluPanda@users.noreply.github.com>
2019-02-19 10:52:22 +0530

    Add files via upload

---
+++ ensembling/bagging.py
@@ -0,0 +1,71 @@
+import util
+import numpy as np
+import sys
+import random
+
+PRINT = True
+
+###### DON'T CHANGE THE SEEDS ##########
+random.seed(42)
+np.random.seed(42)
+
+class BaggingClassifier:
+    """
+    Bagging classifier.
+
+    Note that the variable 'datum' in this code refers to a counter of features
+    (not to a raw samples.Datum).
+    
+    """
+
+    def __init__( self, legalLabels, max_iterations, weak_classifier, ratio, num_classifiers):
+
+        self.ratio = ratio
+        self.num_classifiers = num_classifiers
+        self.classifiers = [weak_classifier(legalLabels, max_iterations) for _ in range(self.num_classifiers)]
+
+    def train( self, trainingData, trainingLabels):
+        """
+        The training loop samples from the data "num_classifiers" time. Size of each sample is
+        specified by "ratio". So len(sample)/len(trainingData) should equal ratio. 
+        """
+
+        self.features = trainingData[0].keys()
+        "*** YOUR CODE HERE ***"
+        sample_size = int(self.ratio * len(trainingData))
+        for i in range(self.num_classifiers):
+            random_integers = np.random.randint(len(trainingData), size = sample_size)
+            sampled_data = []
+            sampled_labels = []
+            for choosen_element in random_integers:
+                sampled_data.append(trainingData[choosen_element])
+                sampled_labels.append(trainingLabels[choosen_element])
+
+            self.classifiers[i].train(sampled_data, sampled_labels, None)
+
+
+
+
+
+
+    def classify( self, data):
+        """
+        Classifies each datum as the label that most closely matches the prototype vector
+        for that label. This is done by taking a polling over the weak classifiers already trained.
+        See the assignment description for details.
+
+        Recall that a datum is a util.counter.
+
+        The function should return a list of labels where each label should be one of legaLabels.
+        """
+
+        "*** YOUR CODE HERE ***"
+        prediction = []
+        for i in range(len(data)):
+            guesses = []
+            for j in range(self.num_classifiers):
+                guesses.append(self.classifiers[j].classify([data[i]])[0])
+
+            prediction.append(int(np.sign(sum(guesses))))
+
+        return prediction
